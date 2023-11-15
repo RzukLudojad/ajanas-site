a3dcda852f54561e029d3a76b3ab309495d6eea1
Rzuk <siemanokolanomnieboli@gmail.com>
2023-11-15 01:48:41 +0100

    animated responsive menu, buttons bold on hover

--- hamburger.js
+++ hamburger.js
@@ -3,10 +3,15 @@ const hamburger = document.querySelector(".burger-icon");
 function menuShow (){
     menu = document.querySelector(".navbar ul")
     if (hamburger.innerHTML === "menu"){
-        menu.style.display = "inline";
-        hamburger.innerHTML = "close";}
+        menu.style.transform = "translateY(0px)";
+        menu.style.opacity = 1;
+        /*menu.style.display = "inline";*/
+        hamburger.innerHTML = "close";
+    }
     else {
-        menu .style.display = "none";
+        /*menu .style.display = "none";*/
+        menu.style.transform = "translateY(-20px)";
+        menu.style.opacity = 0;
         hamburger.innerHTML = "menu";
     };
 
