bc8f7371b57907a510b4d618cbc7214f61892a0a
Rzuk <siemanokolanomnieboli@gmail.com>
2023-11-21 00:31:28 +0100

    Header made into a reusable HTML component Also started working on a UXberries case study page

--- header.js
+++ header.js
@@ -1,4 +1,75 @@
-function elementFromHtml(html){
+class headerNavbar extends HTMLElement {
+	connectedCallback() {
+		this.innerHTML = `
+			<a href='../index.html#'><img src="../images & icons/Ajanas logo.svg" style="height: 3rem;" title ="Homepage" alt="Ajanas"></a>
+			<nav class="navbar">
+				<span class="material-symbols-outlined burger-icon" id="menuIcon">menu</span>
+				<ul>
+					<li><a class="button button-navbar" href="../index.html#aboutMe">About Me</a></li>
+					<li><div id="projects"><a class="button button-navbar" href="../index.html#myProjects" title="Click here to see a list of my projects">My projects</a>
+						<div class="dropdown">
+							<ul>
+								<li><a href="../projects/uxberries.html" class="button button-navbar" title="Case study – students' organization service design">UXberries</a></li>
+								<li><a href="../projects/cometogether.html" class="button button-navbar" title="Case study – obile app for musicians">Come Together</a></li>
+								<li><a href="../projects/work.html" class="button button-navbar" title="Case study — communication audit and improvement in my work">Communication at work</a></li>
+								<li><a href="../projects/kd.html" class="button button-navbar" title="Case study – website for a sports' club">krakowdragons.pl</a></li>
+							</ul>
+						</div>
+					</div></li>
+					<li><a class="button button-navbar">Resume <span class="material-symbols-outlined">open_in_new</span></a></li>
+					<li><a class="button button-navbar button-primary" href="../index.html#contact">Contact me</a></li>
+				</ul>
+			</nav>
+		`
+	}
+}
+
+customElements.define("header-navbar", headerNavbar)
+
+/*
+const template = document.createElement ('template')
+template.innerHTML
+
+const template = `
+<a href='../index.html/#'><img src="../images & icons/Ajanas logo.svg" style="height: 3rem;" title ="Homepage" alt="Ajanas"></a>
+<nav class="navbar">
+	<span class="material-symbols-outlined burger-icon" id="menuIcon">menu</span>
+	<ul>
+		<li><a class="button button-navbar" href="#aboutMe">About Me</a></li>
+		<li><div id="projects"><a class="button button-navbar" href="#myProjects" title="Click here to see a list of my projects">My projects</a>
+			<div class="dropdown">
+				<ul>
+					<li><a href="projects/uxberries.html" class="button button-navbar" title="Case study – students' organization service design">UXberries</a></li>
+					<li><a href="projects/cometogether.html" class="button button-navbar" title="Case study – obile app for musicians">Come Together</a></li>
+					<li><a href="projects/work.html" class="button button-navbar" title="Case study — communication audit and improvement in my work">Communication at work</a></li>
+					<li><a href="projects/kd.html" class="button button-navbar" title="Case study – website for a sports' club">krakowdragons.pl</a></li>
+				</ul>
+			</div>
+		</div></li>
+		<li><a class="button button-navbar">Resume <span class="material-symbols-outlined">open_in_new</span></a></li>
+		<li><a class="button button-navbar button-primary" href="#contact">Contact me</a></li>
+	</ul>
+</nav>
+
+`;*/
+
+
+
+	/*static get observedAttributes() {
+		return ['active'];
+	};
+
+	attributeChangedCallback(name,oldValue,newValue){
+		console.log(name, oldValue, newValue);
+	};
+
+	updateActive(value) {
+		this.
+	}
+};*/
+
+
+/*function elementFromHtml(html){
     const template = document.createElement("template");
     template.innerHTML = html.trim();
     return template.content.firstElementChild;
@@ -25,4 +96,4 @@ const mylist = elementFromHtml(`
 				</ul>
 			</nav>
 `);
-document.body.appendChild(mylist);
+document.body.appendChild(mylist);*/
